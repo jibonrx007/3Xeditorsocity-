376789961218b21f9eaa82d793e40abf420b0429
jibonrx007 <jibonahmed2ndid@gmail.com>
2026-06-28 14:31:27 +0600

    Add 1ndEmoji.js to handle 🥵 emoji messages

---
+++ scripts/cmds/1ndEmoji.js
@@ -0,0 +1,25 @@
+module.exports = {
+  config: {
+    name: "jibonrx",
+    version: "1.0",
+    author: "Goku",
+    countDown: 5,
+    role: 0,
+    shortDescription: "triggered by 🥵",
+    longDescription: "sends Fahad video when 🥵 emoji is sent",
+    category: "no prefix",
+  }, 
+
+  onStart: async function(){}, 
+
+  onChat: async function({ event, message }) {
+    if (event.body && event.body.includes("🥵")) {
+      const video = "https://files.catbox.moe/1ffh5y.mp4";
+
+      return message.reply({
+        body: " 𝐉𝐈𝐁𝐎𝐍 𝐀𝐇𝐌𝐄𝐃 𝐏𝐑𝐄𝐌𝐈𝐔𝐌💫𝐓𝐇𝐈𝐒 𝐐𝐔𝐀𝐋𝐈𝐓𝐘🎬",
+        attachment: await global.utils.getStreamFromURL(video)
+      });
+    }
+  }
+}
